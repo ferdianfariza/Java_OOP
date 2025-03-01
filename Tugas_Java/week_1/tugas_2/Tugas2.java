b1c0d995719728a988b5d57344195002105df5f3
Ferdian Fariza <90488348+ferdianfariza@users.noreply.github.com>
2025-03-01 15:42:22 +0700

    Update Tugas2.java

--- Tugas_Java/week_1/tugas_2/Tugas2.java
+++ Tugas_Java/week_1/tugas_2/Tugas2.java
@@ -9,46 +9,44 @@ public class Tugas2 {
       int sisi = 10;
       int luas = sisi * sisi;
       int keliling = 4 * sisi;
-      System.out.println("Persegi");
-      System.out.println("Sisi      : " + sisi);
-      System.out.println("Luas      : " + luas);
-      System.out.println("Keliling  : " + keliling);
-      System.out.println("\n");
+      System.out.println("Persegi--------------------");
+      System.out.println("Sisi      : " + sisi + "cm");
+      System.out.println("Luas      : " + luas + "cm^2");
+      System.out.println("Keliling  : " + keliling + "cm");
 
       // Segitiga Sama Kaki
       int alas = 6;
       int tinggiSegitiga = 8;
       int sisiMiring = 10;
-      int kelilingSegitiga = sisiMiring + sisiMiring * alas;
-      float luasSegitiga = (1f / 2f) * alas * tinggiSegitiga;
-      System.out.println("Segitiga Sama Kaki");
-      System.out.println("Alas      : " + alas);
-      System.out.println("Tinggi    : " + tinggiSegitiga);
-      System.out.println("SisiMiring: " + sisiMiring);
-      System.out.println("Luas      : " + luasSegitiga);
-      System.out.println("Keliling  : " + kelilingSegitiga);
-      System.out.println("\n");
+      int kelilingSegitiga = sisiMiring + sisiMiring + alas;
+      int luasSegitiga = (alas * tinggiSegitiga) / 2;
+      System.out.println("Segitiga Sama Kaki---------");
+      System.out.println("Alas      : " + alas + "cm");
+      System.out.println("Tinggi    : " + tinggiSegitiga + "cm");
+      System.out.println("SisiMiring: " + sisiMiring + "cm");
+      System.out.println("Luas      : " + luasSegitiga + "cm^2");
+      System.out.println("Keliling  : " + kelilingSegitiga + "cm");
 
       // Lingkaran
       int dlingkaran = 10;
-      float luasLingkaran = (1f / 4f) * phi * (dlingkaran * 2);
+      float luasLingkaran = (1f / 4f) * phi * (dlingkaran * dlingkaran);
       float kelilingLingkaran = phi * dlingkaran;
-      System.out.println("Lingkaran");
-      System.out.println("Diameter  : " + dlingkaran);
-      System.out.println("Luas      : " + luasLingkaran);
-      System.out.println("Keliling  : " + kelilingLingkaran);
-      System.out.println("\n");
+      System.out.println("Lingkaran------------------");
+      System.out.println("Diameter  : " + dlingkaran + "cm");
+      System.out.println("Luas      : " + luasLingkaran + "cm^2");
+      System.out.println("Keliling  : " + kelilingLingkaran + "cm");
 
       // Kubus
       int sKubus = 10;
       int kelilingKubus = 12 * sKubus;
       int luasPermukaanKubus = 6 * (sKubus * sKubus);
       int volumeKubus = sKubus * sKubus * sKubus;
-      System.out.println("Kubus");
-      System.out.println("Sisi            : " + sKubus);
-      System.out.println("Volume          : " + volumeKubus);
-      System.out.println("Keliling Kubus  : " + kelilingKubus);
-      System.out.println("Luas Permukaan  : " + luasPermukaanKubus);
+      System.out.println("Kubus----------------------");
+      System.out.println("Sisi            : " + sKubus + "cm");
+      System.out.println("Volume          : " + volumeKubus + "cm^3");
+      System.out.println("Keliling Kubus  : " + kelilingKubus + "cm");
+      System.out.println("Luas Permukaan  : " + luasPermukaanKubus + "cm^2");
+      System.out.println("---------------------------");
 
    }
 }
