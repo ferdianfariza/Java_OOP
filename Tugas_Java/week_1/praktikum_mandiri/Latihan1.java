837d85be94d3081195212f5a5f5e30ebf079c71b
Ferdian Fariza <90488348+ferdianfariza@users.noreply.github.com>
2025-03-01 15:41:35 +0700

    Update Latihan1.java

--- Tugas_Java/week_1/praktikum_mandiri/Latihan1.java
+++ Tugas_Java/week_1/praktikum_mandiri/Latihan1.java
@@ -3,6 +3,8 @@ package week_1.praktikum_mandiri;
 public class Latihan1 {
       public static void main(String[] args) {
             System.out.println(
-                        "Hallo, nama saya adalah Ferdian Nur Fariza \nSaya adalah mahasiswa Teknik Informatika Universitas Dian Nuswantoro \nSaya sedang menulis program Java");
+                        "Hallo, nama saya adalah Ferdian Nur Fariza\n" +
+                                    "Saya adalah mahasiswa Teknik Informatika Universitas Dian Nuswantoro\n" +
+                                    "Saya sedang menulis program Java");
       }
 }
