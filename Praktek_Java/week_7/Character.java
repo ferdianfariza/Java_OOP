68b9ecbf3ee0ece015f2f5062ffb50fa58992aa5
urpapi <111202315074@mhs.dinus.ac.id>
2025-04-16 11:04:35 +0700

    add week_7

---
+++ Praktek_Java/week_7/Character.java
@@ -0,0 +1,35 @@
+//package praktek_java.week_7;
+//inheritance adalah proses pewarisan sifat dari kelas induk ke kelas anak. yaitu dari superclass ke subclass,
+// kelas itu hanya ada atribut dan method
+// inheritance mewariskan method dan atribut dari superclass ke subclass
+//hp, lvl, exp
+
+public class Character { 
+      protected String nama; //gunakan protected agar bisa diakses oleh subclass dan class itu sendiri
+      protected int level = 0;
+      protected int hp = 0;
+      protected int exp = 0;
+
+      public void Character(){
+         this.nama= "Steve";
+
+      }
+      public void Character(String nama, int level, int hp, int exp){ //overloading constructor
+         this.nama= nama;
+         this.level= level;
+         this.hp= hp;
+         this.exp= exp;
+      }
+
+
+      public int getHp(){
+         return this.hp;
+      }
+      public int getExp(){
+         return this.exp;
+      }
+      public int getLevel(){
+         return this.level;
+      }
+      
+   }
---
+++ Praktek_Java/week_7/Play.java
@@ -0,0 +1,9 @@
+package praktek_java.week_7;
+
+import week_2.Character;
+
+public class Play {
+   public static void main(String[] args){
+      
+   }
+}
